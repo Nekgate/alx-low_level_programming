5de82ea438f6f06f456ec575b6a675b0eb69935d
Nekgate <Nekgate@gmail.com>
2023-09-08 02:06:18 -0700

    Script that prints statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
 *main - A script that prints
-*Return: Alway 1 (Success)
+*
+*Return: Alway 0 (Success)
 */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
