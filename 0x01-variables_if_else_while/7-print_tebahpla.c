3c290887515296f253f789c48f63f7734735526b
Nekgate <Nekgate@gmail.com>
2023-09-08 19:25:12 -0700

    A program that prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - My Entry program
+*Return: Return 0 (Success)
+*/
+int main(void)
+{
+	char b;
+
+	b = 'd';
+	while
+		(b >= 'a') {
+			putchar(b);
+			b--;
+		}
+	putchar('\n');
+	return (0);
+}
