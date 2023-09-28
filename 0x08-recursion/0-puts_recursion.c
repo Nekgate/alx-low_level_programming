84bb50a3cdf63def8361468941a1525e55d592f8
Nekgate <Nekgate@gmail.com>
2023-09-27 23:52:29 -0700

    A function that prints a string, followed by a new line.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*__puts_recursion(char *s) - program that prints string
+*@s: string to print
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
