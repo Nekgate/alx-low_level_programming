8d5aa02a19d4ecd7baeaf6353d7c37e5aa3f3651
Nekgate <Nekgate@gmail.com>
2023-10-02 04:22:17 -0700

    A static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strchr - Entry point
+*@s: parameter1 for input
+*@c: parameter2 for input
+*Return: it returns 0
+*/
+
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
