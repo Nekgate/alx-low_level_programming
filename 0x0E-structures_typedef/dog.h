96ac88ac8dbf696d5b9819fb9f8ac2a7b59bab30
Nekgate <Nekgate@gmail.com>
2023-10-10 09:21:22 -0700

    Struct dog task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+*Struct dog - This defines a new type
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*/
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* __DOG_H__*/
