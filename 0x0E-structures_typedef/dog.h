fca8014a439326edf50d40ea740c3ca5b730544a
Nekgate <Nekgate@gmail.com>
2023-10-10 09:48:54 -0700

    Dog task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,35 @@
-#ifndef __DOG_H__
-#define __DOG_H__
+#ifndef DOG_H
+#define DOG_H
+
 
 /**
-*Struct dog - This defines a new type
-*@name: name of dog
-*@age: age of dog
-*@owner: owner of dog
+*struct dog - The dog's basic info
+*@name: First member
+*@age: Second member
+*@owner: Third member
+*
+*Description: Longer description
 */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
 
-struct dog{
-	char *name;
-	float age;
-	char *owner;
-};
 
-#endif /* __DOG_H__*/
+/**
+*dog_t - typedef for struct dog
+*/
+	typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+
+	#endif
